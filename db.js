c42daf7c08ce4c89e169ec5a46661a7d599fb7cf
oato.o <oat9002@hotmail.com>
2017-08-03 00:16:05 +0700

    Added shouldUpdatePrice

--- db.js
+++ db.js
@@ -1,4 +1,6 @@
-let firebase = require("firebase");
+const firebase = require("firebase");
+const async = require("async");
+
 let app = firebase.initializeApp({
   apiKey: "AIzaSyDCD8IhA1OoJ4852sNuiLahW1l1EHnaiP8",
   authDomain: "goldpricetracking.firebaseapp.com",
@@ -10,14 +12,53 @@ let app = firebase.initializeApp({
 
 let db = app.database();
 
-function addPriceData(buy, sell) {
-  let uid = db.ref().child('price').push().key;
-  db.ref('price/' + uid).set({
-    buy: buy,
-    sell: sell
+function addPrice(buy, sell) {
+  if(shouldAddPrice(buy, sell)) {
+    let uid = db.ref().child('price').push().key;
+    db.ref('price/' + uid).set({
+      buy: buy,
+      sell: sell,
+      created_at: new Date().getTime()
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+  else {
+    console.log("It is not necessary to add the same value with previos one.");
+  }
+}
+
+function shouldAddPrice(buy, sell) {
+  return new Promise((resolve, reject) => {
+    db.ref('price').limitToLast(1).once('value').then(snapshot => {
+      let data = snapshot.val();
+      let id = Object.keys(data)[0];
+      let oldBuy = data[id].buy;
+      let oldSell = data[id].sell;
+      if(buy - oldBuy != 0 || sell - oldSell != 0) {
+        resolve(true);
+      }
+      else {
+        resolve(false);
+      }
+    }).catch(err => {
+      reject(err);
+    })
+  });
+}
+
+function getLatestPrice() {
+  return new Promise((resolve, reject) => {
+    db.ref('price').limitToLast(1).once('value').then(snapshot => {
+      resolve(snapshot.val());
+    }).catch(err => {
+      console.log(err);
+    })
   });
 }
 
 module.exports = {
-  addPriceData
+  addPrice,
+  getLatestPrice,
+  shouldAddPrice
 }
