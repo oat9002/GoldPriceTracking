812ff983a5b4b7cd1870fe042ba2755bed3e794f
oat9002 <oat9002@hotmail.com>
2019-09-27 22:34:01 +0700

    Update

--- web/src/App.js
+++ web/src/App.js
@@ -1,104 +1,108 @@
-import React, { PureComponent } from 'react';
-import Graph from './Graph';
-import GoldTable from './Table';
-import { fetchGoldPrices } from './util/Util';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import TableChart from '@material-ui/icons/TableChart';
-import './App.css';
+import React, { PureComponent } from "react";
+import Graph from "./Graph";
+import GoldTable from "./Table";
+import { fetchGoldPrices } from "./util/Util";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import TableChart from "@material-ui/icons/TableChart";
+import "./App.css";
 
 class App extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      numOfRec: 10,
-      prices: null,
+    constructor(props) {
+        super(props);
+        this.state = {
+            numOfRec: 10,
+            prices: null
+        };
+
+        this.maxPrice = 0;
+        this.minPrice = 0;
     }
 
-    this.maxPrice = 0;
-    this.minPrice = 0;
-  }
-
-  componentDidMount() {
-    this.intializeGraphData();
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if (prevState.numOfRec !== this.state.numOfRec) {
-      this.intializeGraphData();
+    componentDidMount() {
+        this.intializeGraphData();
     }
-  }
 
-  onChangeNumOfRec = (days) => {
-    this.setState({
-      numOfRec: days
-    });
-  }
-
-  intializeGraphData = async () => {
-    try {
-      const goldPrices = await fetchGoldPrices(this.state.numOfRec);
-      const maxAndMin = this.getMaxAndMinPrice(goldPrices);
-
-      this.maxPrice = maxAndMin.max;
-      this.minPrice = maxAndMin.min;
-
-      this.setState({
-        prices: goldPrices,
-      });
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevState.numOfRec !== this.state.numOfRec) {
+            this.intializeGraphData();
+        }
     }
-    catch (err) {
-      console.log(err);
-    }
-  }
 
-  getMaxAndMinPrice = (rawData) => {
-    let price = {
-      min: 0,
-      max: 0
+    onChangeNumOfRec = days => {
+        this.setState({
+            numOfRec: days
+        });
     };
 
-    rawData.forEach((element, idx) => {
-      if (idx === 0) {
-        price.min = element.buy;
-        price.max = element.sell;
-      }
-      else {
-        if (element.buy < price.min) {
-          price.min = element.buy;
-        }
-        if (element.sell > price.max) {
-          price.max = element.sell;
-        }
-      }
-    });
+    intializeGraphData = async () => {
+        try {
+            const goldPrices = await fetchGoldPrices(this.state.numOfRec);
+            const maxAndMin = this.getMaxAndMinPrice(goldPrices);
 
-    return price;
-  }
+            this.maxPrice = maxAndMin.max;
+            this.minPrice = maxAndMin.min;
 
-  render() {
-    return (
-      <div className='content'>
-        <AppBar position="static" color='primary'>
-          <Toolbar>
-            <Typography variant="h4">
-              Hitstory
-            </Typography>
-            <span className="titleIcon">
-              <TableChart fontSize='large' />
-            </span>
-          </Toolbar>
-        </AppBar>
-        <div className='graph'>
-          <Graph prices={this.state.prices} maxPrice={this.maxPrice} minPrice={this.minPrice} />
-        </div>
-        <div className='Table'>
-          <GoldTable numOfRec={this.state.numOfRec} onChangeNumOfRec={this.onChangeNumOfRec} prices={this.state.prices} />
-        </div>
-      </div>
-    );
-  }
+            this.setState({
+                prices: goldPrices
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    getMaxAndMinPrice = rawData => {
+        let price = {
+            min: 0,
+            max: 0
+        };
+
+        rawData.forEach((element, idx) => {
+            if (idx === 0) {
+                price.min = element.buy;
+                price.max = element.sell;
+            } else {
+                if (element.buy < price.min) {
+                    price.min = element.buy;
+                }
+                if (element.sell > price.max) {
+                    price.max = element.sell;
+                }
+            }
+        });
+
+        return price;
+    };
+
+    render() {
+        return (
+            <div className="content">
+                <AppBar position="static" color="primary">
+                    <Toolbar>
+                        <Typography variant="h4">History</Typography>
+                        <span className="titleIcon">
+                            <TableChart fontSize="large" />
+                        </span>
+                    </Toolbar>
+                </AppBar>
+                <div className="graph">
+                    <Graph
+                        prices={this.state.prices}
+                        maxPrice={this.maxPrice}
+                        minPrice={this.minPrice}
+                    />
+                </div>
+                <div className="Table">
+                    <GoldTable
+                        numOfRec={this.state.numOfRec}
+                        onChangeNumOfRec={this.onChangeNumOfRec}
+                        prices={this.state.prices}
+                    />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
