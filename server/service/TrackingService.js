e9b459f5f0441d122b12b5e460032ec77e4b13f6
oat9002 <oat9002@hotmail.com>
2020-03-16 19:51:10 +0700

    Change cron to 1 hour

--- server/service/TrackingService.js
+++ server/service/TrackingService.js
@@ -1,42 +1,47 @@
-'use strict';
-const axios = require('axios');
-const cheerio = require('cheerio');
-const db = require('../dal/db.js');
-const cron = require('node-cron');
-const message = require('./LineService');
+"use strict";
+const axios = require("axios");
+const cheerio = require("cheerio");
+const db = require("../dal/db.js");
+const cron = require("node-cron");
+const message = require("./LineService");
 
 function start() {
-  let buyPrice = 0;
-  let sellPrice = 0;
+    let buyPrice = 0;
+    let sellPrice = 0;
 
-  cron.schedule('*/5 * * * *', () => {
-    axios.get('http://www.goldtraders.or.th')
-      .then(res => {
-        return res.data;
-      })
-      .then(html => {
-        let $ = cheerio.load(html);
-        let bpTemp = $('#DetailPlace_uc_goldprices1_lblBLBuy').text();
-        let spTemp = $('#DetailPlace_uc_goldprices1_lblBLSell').text();
-        buyPrice = parseInt(bpTemp.substring(0, bpTemp.length - 3).replace(',', ''));
-        sellPrice = parseInt(spTemp.substring(0, spTemp.length - 3).replace(',', ''));
-        if (buyPrice !== null && sellPrice !== null) {
-          db.shouldAddPrice(buyPrice, sellPrice).then(shouldAdd => {
-            if (shouldAdd) {
-              db.addPrice(buyPrice, sellPrice);
-            }
-          });
-        } else {
-          console.log('Something wrong in price');
-        }
-      })
-      .catch(err => {
-        console.log(err.stack);
-      });
-  });
-  message.pushMessage();
+    cron.schedule("0 * * * *", () => {
+        axios
+            .get("http://www.goldtraders.or.th")
+            .then(res => {
+                return res.data;
+            })
+            .then(html => {
+                let $ = cheerio.load(html);
+                let bpTemp = $("#DetailPlace_uc_goldprices1_lblBLBuy").text();
+                let spTemp = $("#DetailPlace_uc_goldprices1_lblBLSell").text();
+                buyPrice = parseInt(
+                    bpTemp.substring(0, bpTemp.length - 3).replace(",", "")
+                );
+                sellPrice = parseInt(
+                    spTemp.substring(0, spTemp.length - 3).replace(",", "")
+                );
+                if (buyPrice !== null && sellPrice !== null) {
+                    db.shouldAddPrice(buyPrice, sellPrice).then(shouldAdd => {
+                        if (shouldAdd) {
+                            db.addPrice(buyPrice, sellPrice);
+                        }
+                    });
+                } else {
+                    console.log("Something wrong in price");
+                }
+            })
+            .catch(err => {
+                console.log(err.stack);
+            });
+    });
+    message.pushMessage();
 }
 
 module.exports = {
-  start
+    start
 };
