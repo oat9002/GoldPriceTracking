604acd2a330de8a78b33e4e4010e409c32073013
oat9002 <oat9002@hotmail.com>
2020-12-27 12:15:12 +0700

    :construction: Add log

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,9 +12,11 @@ const whitelist = [
     "http://localhost",
     "https://dg.oatto.com",
     "https://goldpricetracking.web.app",
+    "https://registry.hub.docker.com",
 ];
 const corsOptions: CorsOptions = {
     origin: (origin, callback) => {
+        console.log(origin);
         if (origin && whitelist.indexOf(origin) !== -1) {
             callback(null, true);
         } else {
