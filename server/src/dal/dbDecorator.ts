02b69be6c03c00d8f0ad2b8553e36bb8a4b54cce
oat9002 <oat9002@hotmail.com>
2023-11-12 00:37:27 +0700

    ♻️

--- server/src/dal/dbDecorator.ts
+++ server/src/dal/dbDecorator.ts
@@ -3,10 +3,12 @@ import { Price } from "../models/Price";
 import { User } from "../models/User";
 import { deleteCacheByPrefix, getCache, setCache } from "../service/CacheService";
 
-const cachePrefix = "price";
-const getLaatestPriceCacheKey = createCacheKey(cachePrefix, "getLaatestPrice");
-const getLatestPricesCacheKey = createCacheKey(cachePrefix, "getLatestPrices");
-const getPricesLastByDayCacheKey = createCacheKey(cachePrefix, "getPricesLastByDay");
+const priceCachePrefix = "price";
+const userCachePrefix = "user";
+const getLaatestPriceCacheKey = createCacheKey(priceCachePrefix, "getLaatestPrice");
+const getLatestPricesCacheKey = createCacheKey(priceCachePrefix, "getLatestPrices");
+const getPricesLastByDayCacheKey = createCacheKey(priceCachePrefix, "getPricesLastByDay");
+const getAllUserCacheKey = createCacheKey(userCachePrefix, "getAllUser");
 
 function createCacheKey(...key: string[]): string {
     return `${key.join("_")}`;
@@ -15,7 +17,7 @@ function createCacheKey(...key: string[]): string {
 export async function addPrice(buy: number, sell: number): Promise<void> {
     db.addPrice(buy, sell);
 
-    deleteCacheByPrefix(cachePrefix);
+    deleteCacheByPrefix(priceCachePrefix);
 }
 
 export async function shouldAddPrice(buy: number, sell: number): Promise<boolean> {
@@ -38,10 +40,22 @@ export async function getLatestPrice(): Promise<Price> {
 
 export async function addLineUser(userId: string): Promise<void> {
     db.addLineUser(userId);
+
+    deleteCacheByPrefix(userCachePrefix);
 }
 
 export async function getAllUser(): Promise<User[]> {
-    return db.getAllUser();
+    const cached = getCache<User[]>(getAllUserCacheKey);
+
+    if (cached) {
+        return cached;
+    }
+
+    const users = db.getAllUser();
+
+    setCache(getAllUserCacheKey, users);
+
+    return users;
 }
 
 /* number: number of latest data (0 = all)*/
