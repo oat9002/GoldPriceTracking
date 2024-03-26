89515ba1508cc7ad82a0856743148680f1128d0a
oat9002 <oat9002@hotmail.com>
2024-03-27 00:31:30 +0700

    🚧

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ import * as dbDecorator from "./dal/dbDecorator";
 import * as mackerel from "./service/MackerelServie";
 import * as track from "./service/TrackingService";
 import { StatusCode } from "./util/enums";
+import useSwagger from "./middleware/swagger";
 
 const port = process.env.API_PORT ?? 4000;
 const app = express();
@@ -12,10 +13,56 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+/**
+ * @openapi
+ * /:
+ *  get:
+ *     description: Welcome to GoldpriceTracking
+ *     responses:
+ *          200:
+ *              description: Welcome to GoldpriceTracking
+ */
 app.get("/", (_, res) => {
     res.send("Hello, welcome to GoldpriceTracking.");
 });
 
+/**
+ * @openapi
+ * /prices:
+ *  get:
+ *     description: Get latest prices
+ *     parameters:
+ *          - in: query
+ *              name: number
+ *              schema:
+ *                  type: integer
+ *              description: number of latest prices
+ *              required: false
+ *   responses:
+ *     200:
+ *      description: Get latest prices
+ *    content:
+ *     application/json:
+ *     schema:
+ *     type: array
+ *    items:
+ *    type: object
+ *   properties:
+ *   id:
+ *   type: integer
+ *  price:
+ *  type: number
+ * date:
+ * type: string
+ * example:
+ * id: 1
+ * price: 100
+ * date: 2021-01-01
+ *    400:
+ *     description: number must be more than or equal to 0
+ *   500:
+ *   description: Internal Server Error
+ */
 app.get("/prices", async (req, res) => {
     const numOfLatestPrice: number = parseInt((req.query?.number as string) ?? "-1");
     if (numOfLatestPrice < 0) {
@@ -60,6 +107,8 @@ app.post("/donate", async (_, res) => {
     res.status(StatusCode.NotSupport);
     res.json("Not supported");
 });
+
+useSwagger(app);
 
 app.listen(port, () => {
     console.log("listen to port " + port);
