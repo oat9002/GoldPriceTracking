d548e339bbde0a0eaba4ad55301dbdc228baa8bf
Sarun Rakwijitsil <oat9002@hotmail.com>
2024-10-20 16:53:50 +0700

    Delete server/src/service/ManualOperaionService.ts

--- server/src/service/ManualOperaionService.ts
+++
@@ -1,8 +0,0 @@
-import { notify } from "./messageService";
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function notifyWarningMessage(): Promise<void> {
-    const warningMsg =
-        "กรุ๊ปนี้ใช้เฉพาะเเจ้งเตือนราคาทองเท่านั้น กรุณาอย่าโพสต์ข้อความ รูปภาพ หรือวีดีโอที่ไม่เกี่ยวข้องในกรุ๊ปนี้ 🙂";
-    await notify(warningMsg);
-}
