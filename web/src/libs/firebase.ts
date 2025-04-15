f7588ad809a6bac640d4fdcd2e8bd81d53c1309d
Sarun Rakwijitsil <oat9002@hotmail.com>
2025-04-15 10:32:30 +0700

    Update firebase.ts

--- web/src/libs/firebase.ts
+++ web/src/libs/firebase.ts
@@ -17,8 +17,6 @@ async function initializeFirebase() {
         return firebaseApp;
     }
 
-    console.log("Firebase is initializing");
-
     const firebaseConfig = {
         apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
         authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
