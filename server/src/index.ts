395451d90ac7cd7800d5c06c0ae854bb3dafc03c
oat9002 <oat9002@hotmail.com>
2025-07-14 16:34:34 +0700

    :recycle: remove swagger

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,56 +12,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-/**
- * @openapi
- * /:
- *   get:
- *     description: Welcome to GoldpriceTracking
- *     responses:
- *       200:
- *         description: Welcome to GoldpriceTracking
- */
 app.get("/", (_, res) => {
     res.send("Hello, welcome to GoldpriceTracking.");
 });
 
-/**
- * @openapi
- * /prices:
- *   get:
- *     description: Get latest prices
- *     parameters:
- *         - in: query
- *           name: number
- *           schema:
- *             type: integer
- *           description: number of latest prices
- *           required: false
- *     responses:
- *         200:
- *           description: Get latest prices
- *           content:
- *             application/json:
- *               schema:
- *                   type: array
- *               items:
- *                   type: object
- *                   properties:
- *                     buy:
- *                       type: integer
- *                     sell:
- *                       type: integer
- *                     createdAt:
- *                       type: date
- *                     buyDifferent:
- *                       type: integer
- *                     sellDifferent:
- *                       type: integer
- *         400:
- *           description: number must be more than or equal to 0
- *         500:
- *           description: Internal Server Error
- */
 app.get("/prices", async (req, res) => {
     const numOfLatestPrice: number = parseInt((req.query?.number as string) ?? "-1");
     if (numOfLatestPrice < 0) {
@@ -79,61 +33,12 @@ app.get("/prices", async (req, res) => {
     }
 });
 
-/**
- * @openapi
- * /retrieveAndSavePrice:
- *   get:
- *     description: Retrieve and save latest price
- *   responses:
- *     200:
- *       description: success
- *     500:
- *       description: Internal Server Error
-
- */
 app.get("/retrieveAndSavePrice", async (_, res) => {
     await track.retrieveAndSavePrice();
     res.status(StatusCode.okay);
     res.send("success");
 });
 
-/**
- * @openapi
- * /priceslastday:
- *   get:
- *     description: Get prices of last days
- *     parameters:
- *       - in: query
- *         name: days
- *         schema:
- *           type: integer
- *           description: number of days
- *           required: false
- *     responses:
- *       200:
- *         description: Get prices of last days
- *         content:
- *             application/json:
- *               schema:
- *                   type: array
- *               items:
- *                   type: object
- *                   properties:
- *                     buy:
- *                       type: integer
- *                     sell:
- *                       type: integer
- *                     createdAt:
- *                       type: date
- *                     buyDifferent:
- *                       type: integer
- *                     sellDifferent:
- *                       type: integer
- *       400:
- *         description: days must be more than or equal to 0
- *       500:
- *         description: Internal Server Error
- */
 app.get("/priceslastday", async (req, res) => {
     const days = parseInt((req.query?.days as string) ?? "-1");
     if (days < 0) {
